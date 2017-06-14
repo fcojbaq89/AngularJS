ccb5975c7056f1b14cd22c152cb9194123d55fe6
fcojbaq89 <fjbb@hotmail.com>
2017-06-14 02:23:06 +0100

    Two way data binding y ng model
    Ejercicio de comentarios para ver como se relacionan los datos de la
    vista al controlador

--- controller.js
+++ controller.js
@@ -1,4 +1,19 @@
-var app = angular.module("MyFirstApp",[]);
-app.controller("FisrtControler", function($scope){
-	$scope.nombre = "Francisco Jose";
-});
+angular.module("MyFirstApp",[])
+	.controller("FisrtControler", function($scope){
+		$scope.nombre = "Francisco Jose";
+		$scope.nuevoComentario = {};
+		$scope.comentarios = [
+			{
+				comentario: "Buen tutorial",
+				username: "fcojbaq89"
+			},
+			{
+				comentario: "Malísimo el tutorial",
+				username: "otro_usuario"
+			}
+		];
+		$scope.agregarComentario = function() {
+			$scope.comentarios.push($scope.nuevoComentario);
+			$scope.nuevoComentario = {};
+		}
+	});
