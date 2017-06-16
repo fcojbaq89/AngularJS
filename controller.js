06837c0b39f380169b61f9db7961555e852219df
fcojbaq89 <fjbb@hotmail.com>
2017-06-16 04:18:50 +0100

    ng-repit
    Mejora del comportamiento de los módulos y los controladores
    eeelease enter the commit message for your changes. Lines starting

--- controller.js
+++ controller.js
@@ -1,7 +1,51 @@
-angular.module("MyFirstApp",[])
-	.controller("FisrtControler", function($scope, $http) {
-		$http.get('http://rest-service.guides.spring.io/greeting').
-			then(function(responde) {
-				$scope.greeting = responde.data;
-		});
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope, $http) {
+	$http.get('http://rest-service.guides.spring.io/greeting')
+	.then(function(responde) {
+		$scope.greeting = responde.data;
 	});
+});
+
+app.controller('questionController', function($scope) {
+	//objeto pregunta
+	$scope.pregunta = {
+		id: 1,
+		premisa: '¿Que tipo de Framework es AngularJS?',
+		respuestas: [
+			{
+				id: 1,
+				text: 'MVC',
+				active: 'false'
+			},
+			{
+				id: 2,
+				text: 'MVVM',
+				active: 'false'
+			},
+			{
+				id: 3,
+				text: 'MVR',
+				active: 'false'
+			},
+			{
+				id: 4,
+				text: 'MVW',
+				active: 'false'
+			}
+		]
+	};
+	$scope.marcar = function() {
+		angular.forEach($scope.pregunta.respuestas, function(value, key) {
+			value.active = false;
+		});
+		this.key.active = true;
+	};
+	$scope.respuestas = [];
+	$scope.responder = function() {
+		angular.forEach($scope.pregunta.respuestas, function(respuesta, i) {
+			if (respuesta.active)
+				$scope.respuestas.push({ id:$scope.id, key:respuesta.id });
+		});
+	};
+});
